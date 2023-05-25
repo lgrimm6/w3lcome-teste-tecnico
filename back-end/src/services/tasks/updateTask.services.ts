b0acc4f27161bfeac96e2ee2e54d8c4b2f45ca57
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-25 14:10:18 -0300

    Fix: remove async function and handling extra fields

--- back-end/src/services/tasks/updateTask.services.ts
+++ back-end/src/services/tasks/updateTask.services.ts
@@ -1,12 +1,20 @@
-import { ITaskUpdate } from "../../interfaces/tasks"
+import { ITask, ITaskUpdate } from "../../interfaces/tasks"
 import dataBase from "../../database/index"
 import { AppError } from "../../errors";
-const updateTaskServices = async(data: ITaskUpdate, taskId:Number)=>{
+
+const updateTaskServices = (data: ITaskUpdate, taskId:string):ITask=>{
+    const taskUpdate:ITaskUpdate = {}
+    data.titulo && (taskUpdate.titulo=data.titulo)
+    data.concluida && (taskUpdate.concluida=data.concluida)
+
     const taskIndex = dataBase.findIndex(task => task.id === taskId)
+
     if (taskIndex === -1){
         throw new AppError(404, "Task ID not found");
     }
-    dataBase[taskIndex] = {...dataBase[taskIndex], ...data}
+
+    dataBase[taskIndex] = {...dataBase[taskIndex], ...taskUpdate}
+
     return dataBase[taskIndex]   
 }
 
