5004ca052c8086aa5d286d7f1b03cee1d06073ea
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-25 13:59:31 -0300

    Fix: remove async function and function return type

--- back-end/src/controllers/tasks/createTaskController.controller.ts
+++ back-end/src/controllers/tasks/createTaskController.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import createTaskServices from "../../services/tasks/createTask.services"
-import {ITaskCreate } from "../../interfaces/tasks";
-const createTaskController = async(req:Request, res:Response):Promise<Response>  =>{
+import { ITask, ITaskCreate } from "../../interfaces/tasks";
+
+
+const createTaskController = (req: Request, res: Response):Response<ITask>=> {
     const bodyRequest: ITaskCreate = req.body
-    const createdTask = await createTaskServices(bodyRequest)
+    const createdTask:ITask = createTaskServices(bodyRequest)
     return res.status(201).json(createdTask)
 }
 
