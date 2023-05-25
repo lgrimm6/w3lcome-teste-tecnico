049e20fef8dc250cb7e175c1a678996003f68f48
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-25 14:04:05 -0300

    Fix: remove async function and field required

--- back-end/src/services/tasks/createTask.services.ts
+++ back-end/src/services/tasks/createTask.services.ts
@@ -1,9 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
 import { ITask, ITaskCreate } from "../../interfaces/tasks"
 import dataBase from "../../database/index"
-const createTaskServices = async(data:ITaskCreate)=>{
+import { AppError } from '../../errors';
+
+
+const createTaskServices = ({titulo}: ITaskCreate): ITask => {
+    if (!titulo){
+        throw new AppError(400, "Field ['titulo'] is required")
+    }
     const newTask: ITask = {
-        id: 1,
-        titulo: data.titulo,
+        id: uuidv4(),
+        titulo: titulo,
         concluida: false
     }
     dataBase.push(newTask)
