c4d18dd91f8a5231e1b02f5c6bb0f4b9ad43a39d
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-25 01:32:21 -0300

    Feat: created DELETE route of tasks

--- back-end/src/routes/tasks.routes/tasks.routes.ts
+++ back-end/src/routes/tasks.routes/tasks.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import listAllTasksController from "../../controllers/tasks/listAllTasks.controller";
 import createTaskController from "../../controllers/tasks/createTaskController.controller";
 import updateTaskController from "../../controllers/tasks/updateTask.controller";
+import deleteTaskController from "../../controllers/tasks/deleteTaskController";
 
 const router = Router();
 
 router.get('', listAllTasksController)
 router.post('', createTaskController)
 router.patch('/:id', updateTaskController)
+router.delete('/:id', deleteTaskController)
 
 export default router
