f5bcf39372ece5eabf5998c319c86205152e1c17
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-25 01:38:14 -0300

    Feat: services created from task DELETE route

---
+++ back-end/src/services/tasks/deleteTask.services.ts
@@ -0,0 +1,12 @@
+import dataBase from "../../database/index"
+import { AppError } from "../../errors";
+const deleteTaskServices = (taskId:Number): string | undefined=>{
+    const taskIndex = dataBase.findIndex(task => task.id === taskId)
+    if (taskIndex === -1){
+        throw new AppError(404, "Task ID not found");
+    }
+    dataBase.splice(taskIndex,1)
+    return
+}
+
+export default deleteTaskServices
