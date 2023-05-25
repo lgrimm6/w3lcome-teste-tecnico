5daf722f53756d466ba0bfb3e8b92769dd494eb6
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-24 23:02:21 -0300

    Fix: update to imported interface name

--- back-end/src/controllers/tasks/createTaskController.controller.ts
+++ back-end/src/controllers/tasks/createTaskController.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import createTaskServices from "../../services/tasks/createTask.services"
-import {ITaskBodyRequest } from "../../interfaces/tasks";
+import {ITaskCreate } from "../../interfaces/tasks";
 const createTaskController = async(req:Request, res:Response):Promise<Response>  =>{
-    const bodyRequest: ITaskBodyRequest = req.body
+    const bodyRequest: ITaskCreate = req.body
     const createdTask = await createTaskServices(bodyRequest)
     return res.status(201).json(createdTask)
 }
