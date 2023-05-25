d99a7f58de3ffa16376ff84fa31d87d49a027e1b
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-25 01:43:42 -0300

    Feat: applied AppError for index not found

--- back-end/src/services/tasks/updateTask.services.ts
+++ back-end/src/services/tasks/updateTask.services.ts
@@ -1,13 +1,13 @@
 import { ITaskUpdate } from "../../interfaces/tasks"
 import dataBase from "../../database/index"
+import { AppError } from "../../errors";
 const updateTaskServices = async(data: ITaskUpdate, taskId:Number)=>{
     const taskIndex = dataBase.findIndex(task => task.id === taskId)
     if (taskIndex === -1){
-        return "Task not found"
+        throw new AppError(404, "Task ID not found");
     }
     dataBase[taskIndex] = {...dataBase[taskIndex], ...data}
-    return dataBase[taskIndex]
-    
+    return dataBase[taskIndex]   
 }
 
 export default updateTaskServices
