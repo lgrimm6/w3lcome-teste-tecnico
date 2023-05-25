ce583328ec363216a6aad03f221f844d981cf68b
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-24 23:04:59 -0300

    Feat: instantiated route from PATCH to task

--- back-end/src/routes/tasks.routes/tasks.routes.ts
+++ back-end/src/routes/tasks.routes/tasks.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import listAllTasksController from "../../controllers/tasks/listAllTasks.controller";
 import createTaskController from "../../controllers/tasks/createTaskController.controller";
+import updateTaskController from "../../controllers/tasks/updateTask.controller";
 
 const router = Router();
 
 router.get('', listAllTasksController)
 router.post('', createTaskController)
+router.patch('/:id', updateTaskController)
 
 export default router
