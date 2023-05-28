9d3add8ce70304c24c8a772b010054f0d99cc819
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-28 01:18:24 -0300

    Feat: add new global styles

--- front-end/src/styles/global.ts
+++ front-end/src/styles/global.ts
@@ -1,10 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-:root{
-    --color-primary: #DD8A0D;
-    --color-secondary: #041DFE;
 
+    * {
+    margin:0;
+    padding: 0;
+    outline:0;
+    box-sizing: border-box;
+    list-style: none;
+    }
+
+
+    :root{
+        
+    --color-primary: #DD8A0D;
+    --color-secondary:#ff9900;
 
     --grey-0:#E3E3E3;
     --grey-1:#868e96;
@@ -16,21 +26,63 @@ export default createGlobalStyle`
     --negative: #e83f5b;
 
     background-color: #D9D9D9;
-    
 
-    
-}
+    .heading-1-700 {
+        font-family: 'Bai Jamjuree', sans-serif;
+        font-size: 2rem;
+        font-weight:700;
+        color: var(--color-primary)
+    };
+
+    .heading-2-600 {
+        font-family: 'Bai Jamjuree', sans-serif;
+        text-align: center;
+        font-size: 1.6rem;
+        font-weight:600;
+        color: var(--color-primary);
+     };
+
+     .heading-4-600  {
+        font-family: 'Bai Jamjuree', sans-serif;
+        font-size: 1.2rem;
+        font-weight:600;
+        color: var(--color-secondary)
+
+     };
+
+    .heading-5-500  {
+        font-family: 'Bai Jamjuree', sans-serif;
+        font-size: 1rem;
+        font-weight:500;
+        color: var(--color-grey-1)
+
+    };
+
+     .input-label {
+        font-family: 'Bai Jamjuree', sans-serif;
+        font-size: 1rem;
+        font-weight:500;
+        color: var(--color-grey-2)
+     };
+    }
 
     body{
         width: 100%;
         height: 100vh;
     };
-* {
-    margin:0;
-    padding: 0;
-    outline:0;
-    box-sizing: border-box;
-    list-style: none;
+
+    button{
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        &:hover{
+                
+                background-color: var(--grey-1);
+                color: white;
+                transition: all 0.3s ease
+            }
     }
+    
 
 `
