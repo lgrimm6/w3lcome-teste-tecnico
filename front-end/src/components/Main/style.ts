9ea986473bbf021828b34f2daebd25f360efa6a9
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-28 01:27:23 -0300

    Feat: Created Main component

---
+++ front-end/src/components/Main/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const MainStyled = styled.main`
+    margin: 0 auto;
+    width: 80vw;
+    max-width: 1280px;
+    min-height: 70vh;
+    max-height: 720px;
+    background-color: var(--grey-0);
+
+`
+
+
+export default MainStyled
