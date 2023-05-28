8867d3abdc30a5526bcda96b29f2f72c4cfe1082
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-28 13:53:52 -0300

    Feat: added formState to get form errors

--- front-end/src/components/Modal/ModalForms/FormCreateTask/index.tsx
+++ front-end/src/components/Modal/ModalForms/FormCreateTask/index.tsx
@@ -1,36 +1,47 @@
-import { useContext } from 'react'
-import { TaskContext } from '../../../../Context'
-import { FieldValues, useForm } from "react-hook-form"
-import { ITask } from '../../../../interfaces'
+import { useContext } from "react";
+import { TaskContext } from "../../../../Context";
+import { FieldValues, useForm } from "react-hook-form";
+import { ITask } from "../../../../interfaces";
 
-import api from '../../../../services'
-import Input from '../../../InputLabel'
-import FormStyled from '../style'
+import api from "../../../../services";
+import Input from "../../../InputLabel";
+import FormStyled from "../style";
 
 const FormCreateTask: React.FC = () => {
-    const { register, handleSubmit } = useForm<FieldValues>()
-    const { setIsOpenModalCreate, setTasks, tasks } = useContext(TaskContext)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>();
+  const { setIsOpenModalCreate, setTasks, tasks } = useContext(TaskContext);
 
-    const submit = (formData: FieldValues) => {
-        requestCreateTask(formData)
+  const submit = (formData: FieldValues) => {
+    requestCreateTask(formData);
+  };
+
+  const requestCreateTask = async (data: FieldValues) => {
+    try {
+      const res = await api.post<ITask>("/tasks", data);
+      setTasks([...tasks, res.data]);
+      setIsOpenModalCreate(false);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const requestCreateTask = async (data: FieldValues) => {
-        try {
-            const res = await api.post<ITask>('/tasks', data)
-            setTasks([...tasks, res.data])
-            setIsOpenModalCreate(false)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+  return (
+    <FormStyled onSubmit={handleSubmit(submit)}>
+      <Input
+        label="Titulo: "
+        placeholder="Informe um titulo"
+        name="titulo"
+        errors={errors}
+        register={register}
+        required={true}
+      />
+      <button className="buttonSubmitTask">Criar</button>
+    </FormStyled>
+  );
+};
 
-    return (
-        <FormStyled onSubmit={handleSubmit(submit)}>
-            <Input label="Titulo: " placeholder="Informe um titulo" name="titulo" register={register} />
-            <button className="buttonSubmitTask">Criar</button>
-        </FormStyled>
-    )
-}
-
-export default FormCreateTask
+export default FormCreateTask;
