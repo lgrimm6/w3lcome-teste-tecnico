2a4945b954ddfce45f0b5208a19f102119cc87a5
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-28 01:24:18 -0300

    Feat: Created Footer component

---
+++ front-end/src/components/Footer/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const FooterStyled = styled.footer`
+    width: 100%;
+    height: 50px;
+    box-shadow: 0px -4px 4px rgba(0, 0, 0, 0.25);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+`
+export default FooterStyled
