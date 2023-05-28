5861cd800d6c231206f57ca0425bae0491799f88
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-28 01:24:32 -0300

    Feat: Created Header component

---
+++ front-end/src/components/Header/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  width: 100%;
+  height: 100px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+
+  button {
+    width: 30px;
+    height: 30px;
+    background-color: transparent;
+    border: none;
+  }
+`
+
+
+export default HeaderStyled
