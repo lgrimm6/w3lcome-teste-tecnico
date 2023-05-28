35afd079701e5092822fa76efbc1d0fbe06adb78
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-28 01:23:30 -0300

    Feat: Create component checkbox

---
+++ front-end/src/components/Checkbox/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const CheckBoxStyled = styled.label`
+    display: flex;
+    font-size: 18px;
+    font-weight: 700;
+    input{
+        margin-left: 5px;
+        width: 20px;
+        height: 20px;
+    }
+`
+
+export default CheckBoxStyled
