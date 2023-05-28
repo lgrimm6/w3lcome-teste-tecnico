1f1b906b1a05634516ede2cc7d779d944e31c6a2
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-28 13:52:37 -0300

    Feat: changing interface to receive errors and required and added <span/> for errors

--- front-end/src/components/InputLabel/index.tsx
+++ front-end/src/components/InputLabel/index.tsx
@@ -1,24 +1,38 @@
 import { InputHTMLAttributes } from "react";
-import { FieldValues, UseFormRegister } from "react-hook-form";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 import InputLabelStyled from "./style";
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
-    label: string
-    name: string
-    placeholder: string
-    register: UseFormRegister<FieldValues>
+  label: string;
+  name: string;
+  placeholder: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
 }
 
-const Input: React.FC<IInputProps> = ({ label, name, placeholder, register, ...rest }) => {
-    return <InputLabelStyled className="input-label">
-        {label}
-        <input
-            placeholder={placeholder}
-            {...register(name)}
-            {...rest}
-        />
+const Input: React.FC<IInputProps> = ({
+  label,
+  name,
+  placeholder,
+  register,
+  required,
+  errors,
+  ...rest
+}) => {
+  return (
+    <InputLabelStyled className="input-label">
+      {label}
+      <input
+        placeholder={placeholder}
+        {...register(name, { required: required })}
+        {...rest}
+      />
+      {errors.titulo?.type === "required" && (
+        <span className="error-input-label">titulo é obrigatorio</span>
+      )}
     </InputLabelStyled>
-}
+  );
+};
 
-
-export default Input
+export default Input;
